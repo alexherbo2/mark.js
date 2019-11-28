76d40d4aa1845b5bfd4e2c51cf42638035196e56
Alex Leferry 2 <alexherbo2@gmail.com>
2019-11-28 14:53:16 +0100

    Add methods to remove marks
    – Add pop()
    – Add clear()

--- README.md
+++ README.md
@@ -7,11 +7,15 @@
 ``` javascript
 const mark = new Mark
 mark.on('push', (x, y, index) => console.log('Push', x, y, index))
+mark.on('pop', (x, y, index) => console.log('Pop', x, y, index))
 mark.on('jump', (x, y, index) => console.log('Jump', x, y, index))
+mark.on('clear', () => console.log('Clear'))
 
 mark.push()
 document.scrollingElement.scrollBy({ top: 70 })
 mark.jump()
+mark.pop()
+mark.clear()
 ```
 
 More examples at [Krabby].
@@ -19,12 +23,16 @@ More examples at [Krabby].
 ## Methods
 
 - `push()`: Push mark.
+- `pop()`: Pop mark.
 - `jump()`: Jump to mark.
+- `clear()`: Clear marks.
 
 ## Events
 
 - `push(x, y, index)`: A mark has been saved.
+- `pop(x, y, index)`: A mark has been removed.
 - `jump(x, y, index)`: A mark has been restored.
+- `clear()`: All marks have been removed.
 
 ## References
 
--- scripts/mark.js
+++ scripts/mark.js
@@ -4,7 +4,9 @@ class Mark {
     // Events
     this.events = {}
     this.events['push'] = []
+    this.events['pop'] = []
     this.events['jump'] = []
+    this.events['clear'] = []
   }
   on(type, listener) {
     this.events[type].push(listener)
@@ -20,6 +22,14 @@ class Mark {
     this.marks.push([x, y])
     this.triggerEvent('push', x, y, index)
   }
+  pop() {
+    if (this.marks.length === 0) {
+      return
+    }
+    const [x, y] = this.marks.pop()
+    const index = this.marks.length
+    this.triggerEvent('pop', x, y, index)
+  }
   jump() {
     if (this.marks.length === 0) {
       return
@@ -32,6 +42,10 @@ class Mark {
     const [nextX, nextY] = this.marks[nextIndex]
     Mark.setPosition(nextX, nextY)
     this.triggerEvent('jump', nextX, nextY, nextIndex)
+  }
+  clear() {
+    this.marks = []
+    this.triggerEvent('clear')
   }
   static getPosition() {
     const x = document.documentElement.scrollLeft
